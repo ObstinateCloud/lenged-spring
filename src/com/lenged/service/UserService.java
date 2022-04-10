114f14aae1a1a6bbbed8af33543b8ad76df143d4
zjyselfpc <zjyselfpc@person>
2022-04-10 22:50:17 +0800

    1.模拟依赖注入 2.模拟回调

--- src/com/lenged/service/UserService.java
+++ src/com/lenged/service/UserService.java
@@ -1,26 +1,18 @@
 package com.lenged.service;
 
 import com.lenged.spring.Autowired;
-import com.lenged.spring.BeanNameAware;
 import com.lenged.spring.Component;
 import com.lenged.spring.Scope;
 
 @Component
 //@Scope("prototype")
-public class UserService implements BeanNameAware {
+public class UserService {
 
     @Autowired
     private OrderService orderService;
 
-    String beanName;
 
-
-    public void test(){
+    private void test(){
        System.out.println(orderService);
    }
-
-    @Override
-    public void setBeanName(String beanName) {
-        this.beanName = beanName;
-    }
 }
